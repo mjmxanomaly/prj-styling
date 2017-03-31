fd8552bc6c10c8f22fcd6f53b1eceb4b7a452ee4
mjmxanomaly <mjmxanomaly@gmail.com>
2017-03-31 12:11:54 -0700

    added separate animations file

---
+++ src/app/projects/animations.ts
@@ -0,0 +1,28 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+export const markedTrigger = trigger('markedState', [
+	state('default', style({
+		border: '1px solid black',
+		backgroundColor: 'transparent',
+		padding: '20px'
+	})),
+	state('marked', style({
+		border: '2px solid blue',
+		backgroundColor: '#caeff9',
+		padding: '19px'
+	})),
+	transition('default => marked', [
+		style({
+			border: '2px solid black',
+			padding: '19px'
+		}),
+		animate('300ms ease-out')
+	]),
+	transition('marked => default', [
+		style({
+			border: '1px solid blue',
+			padding: '20px'
+		}),
+		animate('300ms ease-out')
+	])
+]);
